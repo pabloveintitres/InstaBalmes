0dada96e8a80ba6918acc5a0562a552ff134244a
pabloveintitres <pabloveintitres@gmail.com>
2020-04-10 00:42:52 +0200

    0.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="col-3 pb-5">
+            <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-19/s320x320/30589887_2010876322572326_834434668697747456_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_ohc=AzzTIOXMDZ4AX-l7ZmY&oh=8c8317fcb609865cf40c24074086d2c0&oe=5EBA5708" alt="" class="rounded-circle" id="profileImage">
+        </div>
+        <div class="col-9">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
