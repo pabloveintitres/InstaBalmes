cb674b1f83a3b4e2e8f2af538886ef24f7f0084d
pabloveintitres <pabloveintitres@gmail.com>
2020-04-10 05:54:51 +0200

    0.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,10 +24,10 @@
                 <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/71036524_589029411628915_2022576023367012211_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Hzs2EB6AhlkAX8A8-oB&oh=5e86c42ff9970b230074ca131aecf824&oe=5EB7CF98" alt="" class="w-100">
             </div>
             <div class="col-4">
-                <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/71036524_589029411628915_2022576023367012211_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Hzs2EB6AhlkAX8A8-oB&oh=5e86c42ff9970b230074ca131aecf824&oe=5EB7CF98" alt="" class="w-100">
+                <img src=https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/69478559_1300223496844457_7961672726577489446_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=7Z0M6BETbIUAX8CW9zv&oh=bf3dfc55f34c606acddc81cb8dd4e3d0&oe=5EB8EA4E alt="" class="w-100">
             </div>
             <div class="col-4">
-                <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/71036524_589029411628915_2022576023367012211_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=Hzs2EB6AhlkAX8A8-oB&oh=5e86c42ff9970b230074ca131aecf824&oe=5EB7CF98" alt="" class="w-100">
+                <img src="https://instagram.fbcn3-1.fna.fbcdn.net/v/t51.2885-15/e15/c257.0.565.565a/67503282_510013546478555_7857664029003016961_n.jpg?_nc_ht=instagram.fbcn3-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=nCm95oJ95k0AX8VuacX&oh=4819138469df21296f7acd0cae79332d&oe=5EB7D608" alt="" class="w-100">
             </div>
         </div>
     </div>
