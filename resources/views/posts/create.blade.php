e0ab6199f21b836c663b473307c311c31a644e19
pabloveintitres <pabloveintitres@gmail.com>
2020-04-11 18:26:35 +0200

    0.3

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,20 +2,40 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="form-group row">
-                    <label for="caption" class="col-md-4 col-form-label">Post caption</label>
-                        <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" caption="caption" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+        <form action="">
+            <div class="row">
+                <div class="col-8 offset-2">
+                    <div class="form-group row">
+                        <label for="caption" class="col-md-4 col-form-label">Post caption</label>
+                        <input
+                            id="caption"
+                            type="text"
+                            class="form-control @error('caption') is-invalid @enderror"
+                            caption="caption" value="{{ old('caption') }}"
+                            required autocomplete="caption" autofocus>
 
                         @error('caption')
                         <span class="invalid-feedback" role="alert">
-                             <strong>{{ $message }}</strong>
+                            <strong>{{ $message }}</strong>
                          </span>
                         @enderror
+                    </div>
 
+                    <div class="row">
+                      <label for="image" class="col-md-4 col-form-label">Post image</label>
+                       <input type="file" name="image" id="image" class="form-control-file">
+
+                     @error('image')
+                     <span class="invalid-feedback" role="alert">
+                         <strong>{{ $message }}</strong>
+                      </span>
+                    @enderror
+                    </div>
+                        <div class="row">
+                            <button class="btn btn-primary">Add new post</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 @endsection
